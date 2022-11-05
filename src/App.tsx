34045492670a9a7ef8c38539eeb31a28ca3448a6
Cyn Liu <cynthialiustar@gmail.com>
2022-11-05 01:31:03 -0700

    change directions into a modal. Next time: fix post-stop-add-endpoint-deletion bug...

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function Copyright() {
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        cynliu
       </Link>{' '}
       {new Date().getFullYear()}.
     </Typography>
--- src/components/SearchAndRoute.tsx
+++ src/components/SearchAndRoute.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';''
-import { Stack } from '@mui/material';
+import { Button, Modal, Stack } from '@mui/material';
 import AddressBar from './AddressBar';
 import SearchStop from './SearchStop';
 import Directions from './Directions';
@@ -20,13 +20,22 @@ type SearchAndRouteProps = {
 export default function SearchAndRoute({
     stops, startQuery, endQuery, onSearch, onSearchStop, routeResponse
 }: SearchAndRouteProps) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     return (
         <Stack spacing={2} className="left-panel-contents">
             <AddressBar addressLabel='Start' addressContent={startQuery} onSearch={onSearch} />
             <AddressBar addressLabel='End' addressContent={endQuery} onSearch={onSearch} />
             <SearchStop onSearchStop={onSearchStop} disabled={stops.length < 2} />
-            <Directions routeResponse={routeResponse} />
+            <Button onClick={handleOpen} disabled={stops.length < 2}>Get Directions</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+            >
+                <Directions routeResponse={routeResponse} />
+            </Modal>
         </Stack>
     )
 }
